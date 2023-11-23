3e11a684aba4001ff7eb4f84b0c252b8ae267e6d
Lennox Anderson <60410047+lennox55555@users.noreply.github.com>
2023-11-23 01:47:23 -1000

    Update lambda_function.py

--- AWSLambda/lambda_function.py
+++ AWSLambda/lambda_function.py
@@ -3,34 +3,33 @@ import requests
 import lyricsgenius as lg
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-
+import re
 
 
 def lambda_handler(event, context):
     try:
-        #  queryStringParameters 
+        #  queryStringParameters
         song = event.get('queryStringParameters', {}).get('song', None)
-        
+
         album = event.get('queryStringParameters', {}).get('album', None)
-        
+
         artist = event.get('queryStringParameters', {}).get('artist', None)
-        
+
         func = event.get('queryStringParameters', {}).get('func', None)
-        
+
         # check if api was passed a param
         if func is None:
             return {
                 'statusCode': 400,
                 'body': json.dumps({'message': 'Missing parameters'})
             }
-        
+
         # ensure params are strings
         song = str(song) if song is not None else None
-        
-        album = str(album) if album is not None else None
-        
-        artist = str(artist)
 
+        album = str(album) if album is not None else None
+
+        artist = str(artist)
 
         # based on func param and no null values calls specified function
         if func == 'songSentiment':
@@ -85,137 +84,100 @@ def lambda_handler(event, context):
         }
 
 
-
 def songSentiment(song, artist):
-    
-    client_id = 'fc5e07f18cd74c7581d593b7d5790e3d'
-    
-    client_secret = '7ed2f139d7044c0b8d0ef50bb6835d64'
-
-    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-    
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-
-    results = sp.search(q=f'track:{song} artist:{artist}', type='track')
-    
+    # Genius setup
     genius = lg.Genius('rp2cHbcKjKMS5h5oa2o1T8Wpg1ZpPk9qvvT5CnBEInnGDbo1TZ1UvnGJKR5oiNs4')
-   
-    song1 = genius.search_song(song+ " " + artist)
-    
-    listOfRequest = []
-    
-    if results and results['tracks']['items']:
-        track = results['tracks']['items'][0]
-    
-        track_name = track['name']
-        
-        listOfRequest.append(song)
-        
-        artist_name = ', '.join([artist['name'] for artist in track['artists']])
-        
-        listOfRequest.append(artist_name)
-        
-        track_image_url = track['album']['images'][0]['url']
-        
-        listOfRequest.append(track_image_url)
-        
-        listOfRequest.append(song1.lyrics)
-    
-        return listOfRequest
-        
+
+    # Search for the song lyrics with Genius
+    song_lyrics = genius.search_song(song, artist)
+
+    if song_lyrics:
+        # Clean up the lyrics by removing unwanted text (e.g., "Contributors" section)
+        pattern = r".*Contributors"
+        cleaned_lyrics = re.sub(pattern, "", song_lyrics.lyrics, flags=re.DOTALL)
+
+        return cleaned_lyrics
     else:
-        
         return "Could Not Find Song"
-    
+
 
 def albumSentiment(album_name, artist):
-    client_credentials_manager = SpotifyClientCredentials(client_id='fc5e07f18cd74c7581d593b7d5790e3d', client_secret='7ed2f139d7044c0b8d0ef50bb6835d64')
-    
+    # Spotify setup
+    client_credentials_manager = SpotifyClientCredentials(client_id='fc5e07f18cd74c7581d593b7d5790e3d',
+                                                          client_secret='7ed2f139d7044c0b8d0ef50bb6835d64')
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+    # Genius setup
+    genius = lg.Genius('rp2cHbcKjKMS5h5oa2o1T8Wpg1ZpPk9qvvT5CnBEInnGDbo1TZ1UvnGJKR5oiNs4')
+
+    # Search for the album on Spotify
     results = sp.search(q=f"album:{album_name} artist:{artist}", type='album')
 
-    listOfRequest = []
-    
     if results and results['albums']['items']:
-        
         album_id = results['albums']['items'][0]['id']
-
-        album_info = sp.album(album_id)
-        
-        album_image_url = album_info['images'][0]['url']
-        
-        album_artists = [artist['name'] for artist in album_info['artists']]
-        
-        listOfRequest.append(album_artists)
-        
-        listOfRequest.append(album_name)
-        
-        listOfRequest.append(album_image_url)
-
         album_tracks = sp.album_tracks(album_id)
-        
         songs = [track['name'] for track in album_tracks['items']]
-        
-        listOfRequest.append(songs)
-        
-        return listOfRequest
+
+        # Initialize an empty string to store all lyrics
+        all_lyrics = ""
+
+        # Fetch and concatenate lyrics for each song in the album
+        for song_title in songs:
+            song = genius.search_song(song_title, artist)
+            if song:
+                all_lyrics += f"\n{song_title}\n{song.lyrics}\n"
+            else:
+                all_lyrics += f"\n{song_title}\nLyrics not found\n"
+
+        return all_lyrics.strip()
     else:
         return "Could Not Find Album"
 
-
+      
 
 
 def artistSentiment(artist):
-    client_id = 'fc5e07f18cd74c7581d593b7d5790e3d'
-    
-    client_secret = '7ed2f139d7044c0b8d0ef50bb6835d64'
-
-    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-    
+    # Spotify setup
+    client_credentials_manager = SpotifyClientCredentials(client_id='fc5e07f18cd74c7581d593b7d5790e3d',
+                                                          client_secret='7ed2f139d7044c0b8d0ef50bb6835d64')
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-    results = sp.search(q=f'artist:{artist}', type='artist')
+    # Genius setup
+    genius = lg.Genius('rp2cHbcKjKMS5h5oa2o1T8Wpg1ZpPk9qvvT5CnBEInnGDbo1TZ1UvnGJKR5oiNs4')
 
-    listOfRequest = []
-    
-    listOfRequest.append(artist)
-
-    if results and results['artists']['items']:
-        
-        artist = results['artists']['items'][0]
-        
-        artist = results['artists']['items'][0]
-    
-        followers = artist["followers"]["total"]
-    
-        listOfRequest.append(followers)
-    
-        genre = artist["genres"]
-    
-        listOfRequest.append(genre)
-    
-        url = artist["images"][0]["url"]
-    
-        listOfRequest.append(url)
-    
-        top_tracks = sp.artist_top_tracks(artist["id"])
-        
-        track_list = []
-
-        for track in top_tracks['tracks']:
-            song_name = track['name']
-            
-            preview_url = track['preview_url']
-            
-            track_tuple = (song_name, preview_url)
-            
-            track_list.append(track_tuple)
-    
-        return listOfRequest + track_list
-
+    # Fetch artist's information from Spotify
+    results = sp.search(q=f"artist:{artist}", type='artist')
+    if results['artists']['items']:
+        artist_id = results['artists']['items'][0]['id']
     else:
-        return "Artist not found."
+        return "Artist not found"
 
+    all_lyrics = ""
     
+    # Function to process albums and singles
+    def process_albums(albums):
+        nonlocal all_lyrics
+        for album in albums:
+            album_id = album['id']
+            album_tracks = sp.album_tracks(album_id)
+            songs = [track['name'] for track in album_tracks['items']]
+
+            # Fetch and concatenate lyrics for each song
+            for song_title in songs:
+                song = genius.search_song(song_title, artist)
+                if song:
+                    all_lyrics += f"\n{song_title}\n{song.lyrics}\n"
+                else:
+                    all_lyrics += f"\n{song_title}\nLyrics not found\n"
+
+    # Fetch and process albums
+    albums = sp.artist_albums(artist_id, album_type='album')['items']
+    process_albums(albums)
+
+    # Fetch and process singles
+    singles = sp.artist_albums(artist_id, album_type='single')['items']
+    process_albums(singles)
+
+    return all_lyrics.strip()
+
+
